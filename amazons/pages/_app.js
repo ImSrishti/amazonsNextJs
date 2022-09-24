dc2f86afb9769abf15929d50f8dbe974b040ac2b
ImSrishti <shristi998@gmail.com>
2022-09-24 16:28:34 +0530

    material ui issue fix

--- amazons/pages/_app.js
+++ amazons/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    //remove css from server side rendering to fix issue of ssr material ui
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
