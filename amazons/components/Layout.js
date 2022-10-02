00e65cb931109f2a8a5d16e00b1bf614f1dd1747
ImSrishti <shristi998@gmail.com>
2022-10-02 11:45:24 +0530

    api calling mongo db and mongoose

--- amazons/components/Layout.js
+++ amazons/components/Layout.js
@@ -1,23 +1,88 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import React, { useContext } from 'react';
 import Head from 'next/head';
-import React from 'react';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+  Switch,
+  CssBaseline,
+} from '@material-ui/core';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import useStyles from '../utils/styles';
-export default function Layout({ children }) {
+import { Store } from '../utils/Store';
+
+import Cookies from 'js-cookie';
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
   const classes = useStyles();
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF'); //acc to true or false darkmode
+  };
   return (
     <div>
       <Head>
         <title>Amazons</title>
+        <title>{title ? `${title} - Amazons` : 'Amazons'}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <Typography>amazons</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Amazons</Typography>
-      </footer>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Amazons</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
